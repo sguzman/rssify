450aa6fcf678876683ff0ad53674d559998f95d6
Salvador Guzman <guzmansalv@gmail.com>
2025-10-14 07:43:57 -0600

    feat(rust) working stub build

---
+++ target/debug/deps/rssify-d19f3e651a71c710.d
@@ -0,0 +1,5 @@
+/home/dev-marx/Dev/project/rssify/target/debug/deps/rssify-d19f3e651a71c710.d: src/main.rs
+
+/home/dev-marx/Dev/project/rssify/target/debug/deps/rssify-d19f3e651a71c710: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rssify.d
@@ -0,0 +1,1 @@
+/home/dev-marx/Dev/project/rssify/target/debug/rssify: /home/dev-marx/Dev/project/rssify/src/main.rs
