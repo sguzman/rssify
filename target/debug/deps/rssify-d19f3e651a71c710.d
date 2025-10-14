f90f74f9b78bb40b124e8816d595ea7a4df38470
Salvador Guzman <guzmansalv@gmail.com>
2025-10-14 07:44:41 -0600

    fix(target) ignore target

--- target/debug/deps/rssify-d19f3e651a71c710.d
+++
@@ -1,5 +0,0 @@
-/home/dev-marx/Dev/project/rssify/target/debug/deps/rssify-d19f3e651a71c710.d: src/main.rs
-
-/home/dev-marx/Dev/project/rssify/target/debug/deps/rssify-d19f3e651a71c710: src/main.rs
-
-src/main.rs:
--- target/debug/rssify.d
+++
@@ -1,1 +0,0 @@
-/home/dev-marx/Dev/project/rssify/target/debug/rssify: /home/dev-marx/Dev/project/rssify/src/main.rs
